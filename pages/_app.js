43be10dfd367383e9bf94b3447045c41fa852984
Mykola <mykolatymofieiev@gmail.com>
2021-06-09 23:45:50 +0300

    Add base rout, add json file in blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Link from "next/link";
+import Services from "./services";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+    return (
+        <div>
+            <nav>
+                <ul className="menu">
+                    <li><Link href="/"><a>Home</a></Link></li>
+                    <li><Link href="/about"><a>About</a></Link></li>
+                    <li><Link href="/contacts"><a>Contacts</a></Link></li>
+                    <li><Link href="/services"><a>Services</a></Link></li>
+                </ul>
+            </nav>
+            <Component {...pageProps} />
+        </div>
+    )
 }
 
 export default MyApp
