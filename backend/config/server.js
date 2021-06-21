f73947f56c60cbaeb1348b874e91d089a9a580b5
Mykola <mykolatymofieiev@gmail.com>
2021-06-21 16:24:27 +0300

    Add backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd58b3bc78deaa4caff257f9a31bc0176'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '57acd590-0a38-43ed-b556-d86d3f241665'
+};
