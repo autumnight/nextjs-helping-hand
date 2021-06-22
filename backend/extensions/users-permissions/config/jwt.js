a6f8e8f65422155dc8dd7a2916672200d4222a23
Mykola <mykolatymofieiev@gmail.com>
2021-06-22 19:58:44 +0300

    Add file

--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '03fc63a4-68a3-4dbf-a85b-83b2409f8bda'
-};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b4148df0-fd70-46a5-bea0-f7b490b830b4'
+};
