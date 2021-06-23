a88376b7c47fbcfdd4280feec7c9a9a99b9a6185
Mykola <mykolatymofieiev@gmail.com>
2021-06-23 15:17:56 +0300

    Add cloudinary in strapi

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,30 @@
 import MainLayout from '../components/MainLayout'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <MainLayout >
       Index page
+
+      <div>
+        <Image
+          src="/vercel.svg"
+          alt="Galaxy"
+          width={1000}
+          height={750}
+        />
+        <h3>Local Image</h3>
+      </div>
+
+      <div>
+        <Image
+          src="https://res.cloudinary.com/fay/image/upload/v1617047570/galaxy_ne5p8f.jpg"
+          alt="Galaxy"
+          width={1000}
+          height={750}
+        />
+        <h3>Cloudinary - Static</h3>
+      </div>
     </MainLayout>
   )
 }
