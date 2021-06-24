2b563a0248c61dbc8a733efa055aa8a920c415c3
Mykola <mykolatymofieiev@gmail.com>
2021-06-24 19:21:46 +0300

    Add base component

--- frontend/components/MainLayout.js
+++ frontend/components/MainLayout.js
@@ -1,7 +1,22 @@
-export default function MainLayout() {
-  return(
-    <>
+import Head from 'next/head';
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
-    </>
-  )
+export default function MainLayout({ children, title="Base Title" }) {
+    return (
+        <>
+            <Head>
+                <title>{title} | Page</title>
+                <link rel="icon" href="/images/favicon.png"/>
+            </Head>
+
+            <Header/>
+
+            <main>
+                { children }
+            </main>
+
+            <Footer/>
+        </>
+    )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,11 @@
-import '../styles/globals.css';
-import MainLayout from '../components/MainLayout';
+
+import '../styles/base.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MainLayout>
+    <>
       <Component {...pageProps} />
-    </MainLayout>
+    </>
   )
 }
 
